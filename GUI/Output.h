ab28d24ee629a1de52bddcacbe839a67550caf7d
Ammar Nasser <ammarnasser0012@gmail.com>
2021-05-27 15:41:58 +0200

    add a function to draw clean status bar adjust the text to be in good place in the bar

--- GUI/Output.h
+++ GUI/Output.h
@@ -13,15 +13,14 @@ public:
 	Output();		
 
 	window* CreateWind(int, int, int , int) const; //creates the application window
+	Input* CreateInput()  const;		//creates a pointer to the Input object	
 
-	void drawToolBar()  const;		//drawing the toolbar according to the UI.InterfaceMode
-	void clearToolBar() const;		//clears the toolbar
+	void drawToolBar()  const;			//drawing the toolbar according to the UI.InterfaceMode
+	void clearToolBar() const;			//clears the toolbar
+	void drawCleanStatusBar() const;	//Clears the status bar
+	void ClearDrawArea()  const;		//Clears the drawing area
 
-	void CreateStatusBar() const;	//create the status bar
 
-	Input* CreateInput()  const;	//creates a pointer to the Input object	
-	void ClearStatusBar() const;	//Clears the status bar
-	void ClearDrawArea()  const;	//Clears the drawing area
 	
 	// -- Figures Drawing functions
 	void DrawRect(Point P1, Point P2, GfxInfo RectGfxInfo, bool selected=false) const;  //Draw a rectangle
