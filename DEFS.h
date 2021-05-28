95ff15cf4f779f87c3bb9744476cee4f7cf945ee
Ammar Nasser <ammarnasser0012@gmail.com>
2021-05-28 06:12:14 +0200

    rearrange the actions in ActionType and RE_PLAY action

--- DEFS.h
+++ DEFS.h
@@ -6,6 +6,16 @@
 //This file contais some global constants and definitions to be used in the project.
 enum ActionType //The actions supported (you can add more if needed)
 {
+	//Actions related to the GUI
+	DRAWING_AREA,	//A click on the drawing area
+	STATUS,			//A click on the status bar
+	EMPTY,			//A click on empty place in the toolbar
+
+	//Switch actions
+	TO_DRAW,		//Switch interface to Draw mode
+	TO_PLAY,		//Switch interface to Play mode
+
+	//Draw mode actions
 	DRAW_LINE,		//Draw Line
 	DRAW_RECT,		//Draw Rectangle
 	DRAW_TRI,		//Draw Triangle
@@ -13,6 +23,7 @@ enum ActionType //The actions supported (you can add more if needed)
 	CHNG_DRAW_CLR,	//Change the drawing color
 	CHNG_FILL_CLR,	//Change the filling color
 	CHNG_BK_CLR,	//Change background color
+	SELECT,         //Selects figure(s)
 	DEL,			//Delete a figure(s)
 	MOVE,			//Move a figure(s)
 	RESIZE,			//Resize a figure(s)
@@ -21,16 +32,19 @@ enum ActionType //The actions supported (you can add more if needed)
 	BRNG_FRNT,		//Bring a figure to the front of all figures
 	SAVE,			//Save the whole graph to a file
 	LOAD,			//Load a graph from a file
+	REDO,           //Undo action
+	UNDO,           //Redo action
+
+	//Play mode actions
+	RE_PLAY,		//RePlay the game
+	SHAPE_ONLY,     //Choose shape only in the play mode
+	CLR_ONLY,       //Choose color only in the play mode
+	SHAPE_N_CLR,    //Choose shape and color in the play mode
+	AREA,           //Choose area in the play mode
+
+	//Common action in draw and play 
 	EXIT,			//Exit the application
 
-	DRAWING_AREA,	//A click on the drawing area
-	STATUS,			//A click on the status bar
-	EMPTY,			//A click on empty place in the toolbar
-
-	TO_DRAW,		//Switch interface to Draw mode
-	TO_PLAY			//Switch interface to Play mode
-
-	///TODO: Add more action types (if needed)
 };
 
 struct Point	//To be used for figures points
