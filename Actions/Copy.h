afd5c9af8a0ca6af2897893a0912ffdb96c3e9ff
Ammar Nasser <ammarnasser0012@gmail.com>
2021-05-31 10:43:49 +0200

    add copy and paste actions

---
+++ Actions/Copy.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Action.h"
+#include "..\Figures\CFigure.h"
+class Copy : public Action
+{
+private:
+	CFigure** selectedFigures;//pointer to the selected figure that will be copied
+	CFigure** copiedFigures;//pointer to the copied figures
+	CFigure* copyOfFirstSelectedFigure;//pointer to the COPY OF first selected figure
+	CFigure* firstSelectedFig;//pointer to the first selected figure to copy
+	const int nOfFigsToCopy;//number of figures to copy
+public:
+	//constructor
+	Copy(ApplicationManager* pApp, CFigure** selectedFigures, const int size, CFigure* firstFigure);
+
+	virtual void ReadActionParameters();
+
+	virtual void Execute();
+
+	//return the copied figures, this function is neccessary to be passed to the Paste Action constructor
+	virtual CFigure** getCopiedFigures() const;
+	//return the first copied figure, this function is neccessary to be passed to the Paste Action constructor
+	virtual CFigure* getFirstSelectedCopy() const;
+	//return the size of the copied array
+	virtual int getNOfFigsToCopy()const;
+
+	virtual ~Copy();
+};
---
+++ Actions/Paste.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Action.h"
+#include "Move.h"
+class Paste : public Action
+{
+private:
+	CFigure** copiedFigures;//pointer to the copied Figures returned by Copy
+	CFigure* firstCopiedfig;//pointer to the first one copied figure returned by Copy
+	Move* moveAction;//paste needs move action to do the job
+	int nOfCopiedFigs;//size of the copied figures
+public:
+	Paste(ApplicationManager* pApp, CFigure** copiedFigs, const int nCopied, CFigure* firstCopied); //constructor
+
+	//Reads parameters required for action to execute (code depends on action type)
+	virtual void ReadActionParameters();
+
+	//Execute action (code depends on action type)
+	virtual void Execute() ;
+
+	virtual ~Paste();
+};
