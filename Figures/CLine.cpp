4d54ac74105b9fdb312fd6a88758bd2b13ed9d01
Ammar Nasser <ammarnasser0012@gmail.com>
2021-06-01 12:36:13 +0200

    bug in isInsideMe must be fixed

--- Figures/CLine.cpp
+++ Figures/CLine.cpp
@@ -32,7 +32,9 @@ bool CLine::isInsideMe(Point p) const
 	{
 		float grad1 = (float(finish.y - start.y) / (finish.x - start.x));
 		float grad2 = (float(p.y - start.y) / (p.x - start.x));
+		//bug
 		return std::abs(grad1 - grad2) >= 0.0f && std::abs(grad1 - grad2) <= 0.1f;
+		//
 	}
 	return false;
 	
