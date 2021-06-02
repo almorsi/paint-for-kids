69eb6cdfad186a63bd0d95b0b0ef27751c9169b6
Ammar Nasser <ammarnasser0012@gmail.com>
2021-06-02 10:40:53 +0200

    using double inheritance to add game shape and color

---
+++ Actions/GameColorShape.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "GameColorOnly.h"
+#include "GameShapeOnly.h"
+
+class GameColorShape : public GameColorOnly, public GameShapeOnly
+{
+private:
+	std::string shapeColorLabel;
+public:
+	GameColorShape(ApplicationManager* pApp, CFigure** figs, const int size, ActionType& actionAfterPlay);
+	virtual bool isRightChoise(CFigure* fig) const;
+};
