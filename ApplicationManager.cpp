872d0cc6fbbba9733200cb4c685872d4c39ee984
Mohammad Ahmad <mahmodyeha@gmail.com>
2021-06-11 18:52:12 +0200

    Application manager documented

--- ApplicationManager.cpp
+++ ApplicationManager.cpp
@@ -37,8 +37,7 @@ ActionType ApplicationManager::GetUserAction(Point& p) const
 	//Ask the input to get the action from the user.
 	return pIn->GetUserAction(p);		
 }
-////////////////////////////////////////////////////////////////////////////////////
-//Creates an action and executes it
+
 void ApplicationManager::ExecuteAction(ActionType ActType) 
 {
 	Action* pAct = NULL;
@@ -186,139 +185,8 @@ void ApplicationManager::ExecuteAction(ActionType ActType)
 		pAct = NULL;
 	}
 }
-//==================================================================================//
-//						Figures Management Functions								//
-//==================================================================================//
 
-//Add a figure to the list of figures
-void ApplicationManager::AddFigure(CFigure* pFig)
-{
-	if(FigCount < MaxFigCount )
-		FigList[FigCount++] = pFig;	
-}
-//delete figures from figList
-void ApplicationManager::deleteFigures(CFigure** figsToDelete, const int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		assert(figsToDelete[i] != NULL);
-
-		delete GetFigure(figsToDelete[i]); //free figure's memory
-		GetFigure(figsToDelete[i]) = NULL; //null it in figList
-		figsToDelete[i] = NULL;//must be nulled here also, to avoid unselect deleted figure 
-	}
-	//rearrange FigList
-	reArrangeFigList(size);
-	//update figCount
-	FigCount -= size;
-}
-void ApplicationManager::reArrangeFigList(int deletedFigsCount)
-{
-	int nOfUnDeletedFigs = FigCount - deletedFigsCount;
-
-	//figList is called by deleteFigures and there are some figures need to be reArranged
-	//first getting the notNulled figures from figList
-	CFigure** notNulledFigures = new CFigure * [nOfUnDeletedFigs]();//initialize them to NULL
-	for (int i = 0, j = 0; i < FigCount && j < nOfUnDeletedFigs; i++)
-	{
-		if (FigList[i] != NULL)//store it in notNulledFigures
-		{
-			notNulledFigures[j] = FigList[i];
-			FigList[i] = NULL;
-			j++;
-		}
-	}
-	//reArrange figList to only contain the notNUlledfigures
-	for (int i = 0; i < nOfUnDeletedFigs; i++)
-	{
-		FigList[i] = notNulledFigures[i];
-	}
-	delete[] notNulledFigures;
-}
-bool ApplicationManager::isSmallestArea(CFigure* fig) const
-{
-	for (int i = 0; i < FigCount; i++)
-		if (!FigList[i]->isHidden() && fig->getArea() > FigList[i]->getArea())
-			return false;
-	return true;
-}
-bool ApplicationManager::isLargestArea(CFigure* fig) const
-{
-	for (int i = 0; i < FigCount; i++)
-		if (!FigList[i]->isHidden() && fig->getArea() < FigList[i]->getArea())
-			return false;
-	return true;
-}
-////////////////////////////////////////////////////////////////////////////////////
-CFigure *ApplicationManager::GetFigure(Point p) const
-{
-	//If a figure is found return a pointer to it.
-	//if this point (x,y) does not belong to any figure return NULL
-
-	for (int i = FigCount - 1; i >= 0; i--)
-	{
-		if (FigList[i]->isInsideMe(p))
-			return FigList[i];
-	}
-
-	return NULL;
-}
-CFigure* ApplicationManager::GetFigure(int index) const
-{
-	if(index < FigCount)
-		return FigList[index];
-
-	assert(false);
-	return NULL;
-}
-CFigure*& ApplicationManager::GetFigure(CFigure* fig) 
-{
-	
-	for (int i = 0; i < FigCount; i++)
-		if (FigList[i] == fig)
-			return FigList[i];
-
-	assert(false);
-	return fig;
-}
-int ApplicationManager::getFigCount() const
-{
-	return FigCount;
-}
-int ApplicationManager::getIndexOf(CFigure* fig) const
-{
-	if (fig != NULL)
-	{
-		for (int i = 0; i < FigCount; i++)
-			if (fig == FigList[i])
-				return i;
-	}
-	return -1;
-}
-int ApplicationManager::FromClrToInt(color c)
-{
-	if (c.ucBlue == BLACK.ucBlue && c.ucGreen == BLACK.ucGreen && c.ucRed == BLACK.ucRed)
-		return 0;
-	else if (c.ucBlue == GRAY.ucBlue && c.ucGreen == GRAY.ucGreen && c.ucRed == GRAY.ucRed)
-		return 1;
-	else if (c.ucBlue == BLUE.ucBlue && c.ucGreen == BLUE.ucGreen && c.ucRed == BLUE.ucRed)
-		return 2;
-	else if (c.ucBlue == CYAN.ucBlue && c.ucGreen == CYAN.ucGreen && c.ucRed == CYAN.ucRed)
-		return 3;
-	else if (c.ucBlue == GREEN.ucBlue && c.ucGreen == GREEN.ucGreen && c.ucRed == GREEN.ucRed)
-		return 4;
-	else if (c.ucBlue == YELLOW.ucBlue && c.ucGreen == YELLOW.ucGreen && c.ucRed == YELLOW.ucRed)
-		return 5;
-	else if (c.ucBlue == BROWN.ucBlue && c.ucGreen == BROWN.ucGreen && c.ucRed == BROWN.ucRed)
-		return 6;
-	else if (c.ucBlue == ORANGE.ucBlue && c.ucGreen == ORANGE.ucGreen && c.ucRed == ORANGE.ucRed)
-		return 7;
-	else if (c.ucBlue == RED.ucBlue && c.ucGreen == RED.ucGreen && c.ucRed == RED.ucRed)
-		return 8;
-	else //if called by color then 9 means white if it is called by fill the it is nofill
-		return 9;
-}
-void ApplicationManager::saveData(ofstream &OutFile)
+void ApplicationManager::saveData(ofstream& OutFile)
 {
 	OutFile << FromClrToInt(UI.DrawColor) << "," << FromClrToInt(UI.FillColor) << "," << FromClrToInt(UI.BkGrndColor) << std::endl;
 	OutFile << FigCount << std::endl;
@@ -327,29 +195,7 @@ void ApplicationManager::saveData(ofstream &OutFile)
 		FigList[i]->Save(OutFile);
 	}
 }
-color ApplicationManager::FromIntToClr(int n)
-{
-	if (n == 0)
-		return BLACK;
-	else if (n == 1)
-		return GRAY;
-	else if (n == 2)
-		return BLUE;
-	else if (n == 3)
-		return CYAN;
-	else if (n == 4)
-		return GREEN;
-	else if (n == 5)
-		return YELLOW;
-	else if (n == 6)
-		return BROWN;
-	else if (n == 7)
-		return ORANGE;
-	else if (n == 8)
-		return RED;
-	else
-		return WHITE;
-}
+
 void ApplicationManager::loadData(ifstream& Infile)
 {
 	std::string line;
@@ -399,7 +245,7 @@ void ApplicationManager::loadDataFigs(vector<string>& data)
 		FigGfxInfo.BorderWdth = pOut->getCrntPenWidth();
 		FigGfxInfo.FillClr = WHITE;
 		FigGfxInfo.isFilled = false;
-		CFigure* fig = new CLine(ID,start, finish, FigGfxInfo);
+		CFigure* fig = new CLine(ID, start, finish, FigGfxInfo);
 		AddFigure(fig);
 	}
 	else if (std::stoi(data[0]) == 1)
@@ -424,7 +270,7 @@ void ApplicationManager::loadDataFigs(vector<string>& data)
 			FigGfxInfo.isFilled = true;
 		}
 		FigGfxInfo.BorderWdth = pOut->getCrntPenWidth();
-		CFigure* fig = new CRectangle(ID,Corner1, Corner2, FigGfxInfo);
+		CFigure* fig = new CRectangle(ID, Corner1, Corner2, FigGfxInfo);
 		AddFigure(fig);
 	}
 	else if (std::stoi(data[0]) == 2)
@@ -452,13 +298,13 @@ void ApplicationManager::loadDataFigs(vector<string>& data)
 		}
 
 		FigGfxInfo.BorderWdth = pOut->getCrntPenWidth();
-		CFigure* fig = new CTriangle(ID,point1, point2, point3,FigGfxInfo);
+		CFigure* fig = new CTriangle(ID, point1, point2, point3, FigGfxInfo);
 		AddFigure(fig);
 	}
 	else
 	{
 		Point center;
-		int radius,ID;
+		int radius, ID;
 		GfxInfo FigGfxInfo;
 		ID = std::stoi(data[1]);
 		center.x = std::stoi(data[2]);
@@ -476,10 +322,140 @@ void ApplicationManager::loadDataFigs(vector<string>& data)
 			FigGfxInfo.isFilled = true;
 		}
 		FigGfxInfo.BorderWdth = pOut->getCrntPenWidth();
-		CFigure* fig = new CCircle(ID,center, radius, FigGfxInfo);
+		CFigure* fig = new CCircle(ID, center, radius, FigGfxInfo);
 		AddFigure(fig);
 	}
 }
+
+////////////////////////////////////////////////////////////////////////////////////
+//Creates an action and executes it
+//==================================================================================//
+//						Figures Management Functions								//
+//==================================================================================//
+
+//Add a figure to the list of figures
+void ApplicationManager::AddFigure(CFigure* pFig)
+{
+	if(FigCount < MaxFigCount )
+		FigList[FigCount++] = pFig;	
+}
+//delete figures from figList
+void ApplicationManager::deleteFigures(CFigure** figsToDelete, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		assert(figsToDelete[i] != NULL);
+
+		delete GetFigure(figsToDelete[i]); //free figure's memory
+		GetFigure(figsToDelete[i]) = NULL; //null it in figList
+		figsToDelete[i] = NULL;//must be nulled here also, to avoid unselect deleted figure 
+	}
+	//rearrange FigList
+	reArrangeFigList(size);
+	//update figCount
+	FigCount -= size;
+}
+void ApplicationManager::reArrangeFigList(int deletedFigsCount)
+{
+	int nOfUnDeletedFigs = FigCount - deletedFigsCount;
+
+	//figList is called by deleteFigures and there are some figures need to be reArranged
+	//first getting the notNulled figures from figList
+	CFigure** notNulledFigures = new CFigure * [nOfUnDeletedFigs]();//initialize them to NULL
+	for (int i = 0, j = 0; i < FigCount && j < nOfUnDeletedFigs; i++)
+	{
+		if (FigList[i] != NULL)//store it in notNulledFigures
+		{
+			notNulledFigures[j] = FigList[i];
+			FigList[i] = NULL;
+			j++;
+		}
+	}
+	//reArrange figList to only contain the notNUlledfigures
+	for (int i = 0; i < nOfUnDeletedFigs; i++)
+	{
+		FigList[i] = notNulledFigures[i];
+	}
+	delete[] notNulledFigures;
+}
+CFigure* ApplicationManager::GetFigure(Point p) const
+{
+	//If a figure is found return a pointer to it.
+	//if this point (x,y) does not belong to any figure return NULL
+
+	for (int i = FigCount - 1; i >= 0; i--)
+	{
+		if (FigList[i]->isInsideMe(p))
+			return FigList[i];
+	}
+
+	return NULL;
+}
+CFigure* ApplicationManager::GetFigure(int index) const
+{
+	if (index < FigCount)
+		return FigList[index];
+
+	assert(false);
+	return NULL;
+}
+CFigure*& ApplicationManager::GetFigure(CFigure* fig)
+{
+
+	for (int i = 0; i < FigCount; i++)
+		if (FigList[i] == fig)
+			return FigList[i];
+
+	assert(false);
+	return fig;
+}
+int ApplicationManager::FromClrToInt(color c)
+{
+	if (c.ucBlue == BLACK.ucBlue && c.ucGreen == BLACK.ucGreen && c.ucRed == BLACK.ucRed)
+		return 0;
+	else if (c.ucBlue == GRAY.ucBlue && c.ucGreen == GRAY.ucGreen && c.ucRed == GRAY.ucRed)
+		return 1;
+	else if (c.ucBlue == BLUE.ucBlue && c.ucGreen == BLUE.ucGreen && c.ucRed == BLUE.ucRed)
+		return 2;
+	else if (c.ucBlue == CYAN.ucBlue && c.ucGreen == CYAN.ucGreen && c.ucRed == CYAN.ucRed)
+		return 3;
+	else if (c.ucBlue == GREEN.ucBlue && c.ucGreen == GREEN.ucGreen && c.ucRed == GREEN.ucRed)
+		return 4;
+	else if (c.ucBlue == YELLOW.ucBlue && c.ucGreen == YELLOW.ucGreen && c.ucRed == YELLOW.ucRed)
+		return 5;
+	else if (c.ucBlue == BROWN.ucBlue && c.ucGreen == BROWN.ucGreen && c.ucRed == BROWN.ucRed)
+		return 6;
+	else if (c.ucBlue == ORANGE.ucBlue && c.ucGreen == ORANGE.ucGreen && c.ucRed == ORANGE.ucRed)
+		return 7;
+	else if (c.ucBlue == RED.ucBlue && c.ucGreen == RED.ucGreen && c.ucRed == RED.ucRed)
+		return 8;
+	else //if called by color then 9 means white if it is called by fill the it is nofill
+		return 9;
+}
+color ApplicationManager::FromIntToClr(int n)
+{
+	if (n == 0)
+		return BLACK;
+	else if (n == 1)
+		return GRAY;
+	else if (n == 2)
+		return BLUE;
+	else if (n == 3)
+		return CYAN;
+	else if (n == 4)
+		return GREEN;
+	else if (n == 5)
+		return YELLOW;
+	else if (n == 6)
+		return BROWN;
+	else if (n == 7)
+		return ORANGE;
+	else if (n == 8)
+		return RED;
+	else
+		return WHITE;
+}
+
 //==================================================================================//
 //							Interface Management Functions							//
 //==================================================================================//
@@ -501,6 +477,41 @@ Input *ApplicationManager::GetInput() const
 Output *ApplicationManager::GetOutput() const
 {	return pOut; }
 ////////////////////////////////////////////////////////////////////////////////////
+
+//==================================================================================//
+//							Play Mode functions          							//
+//==================================================================================//
+bool ApplicationManager::isSmallestArea(CFigure* fig) const
+{
+	for (int i = 0; i < FigCount; i++)
+		if (!FigList[i]->isHidden() && fig->getArea() > FigList[i]->getArea())
+			return false;
+	return true;
+}
+bool ApplicationManager::isLargestArea(CFigure* fig) const
+{
+	for (int i = 0; i < FigCount; i++)
+		if (!FigList[i]->isHidden() && fig->getArea() < FigList[i]->getArea())
+			return false;
+	return true;
+}
+////////////////////////////////////////////////////////////////////////////////////
+ 
+int ApplicationManager::getFigCount() const
+{
+	return FigCount;
+}
+int ApplicationManager::getIndexOf(CFigure* fig) const
+{
+	if (fig != NULL)
+	{
+		for (int i = 0; i < FigCount; i++)
+			if (fig == FigList[i])
+				return i;
+	}
+	return -1;
+}
+///////////////////////////////////////////////////////////////////////////////////
 //Destructor
 ApplicationManager::~ApplicationManager()
 {
